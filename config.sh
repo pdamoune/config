dfccc309fb9122ad34aa5f6935448f97d1a019b1
Philippe Damoune <pdamoune@student.42.fr>
2018-05-16 01:25:47 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/zsh
 
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/pdamoune/config/master/config.sh)"
